f44383a0f7af0f4974b45f2ded90362a830e9b23
Minhaj Ahmed Moin <31467353+mam98@users.noreply.github.com>
2018-09-15 23:21:28 +0500

    Update Solution.py

--- Week 1/Solution.py
+++ Week 1/Solution.py
@@ -1,9 +1,9 @@
 def func1(lst):
     maxLength = 0;
     for i in lst:
-        if (len(i) > maxLength) : maxLength = len(i)
+        if (len(i) > maxLength) : maxLength = len(i);
     print("* " * maxLength);
-    for i in lst: print ("*", i + " " * (maxLength - (len(i))), "*")
+    for i in lst: print ("*", i + " " * (maxLength - (len(i))), "*");
     print("* " * maxLength);
 
 def func2():
@@ -11,7 +11,7 @@ def func2():
     for i in range(99, -1, -1):
         if (i >= 1):
             if i == 1:
-                bot = bot[:-1]
+                bot = bot[:-1];
             print ( i, bot,"of beer on the wall,", i, bot,"of beer.\nTake one down and pass it around,", i-1, bot,"of beer on the wall.\n\n" )
         else:
             print ( "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall")
